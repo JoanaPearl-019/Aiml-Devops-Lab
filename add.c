2aff890e770eef3db647afe37cfdaf29a0b3d29b
JoanaPearl-019 <245123748019@mvsrec.edu.in>
2026-04-10 12:40:37 +0530

    Add program to calculate sum of four numbers

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30, d = 40;
+    int sum;
+
+    sum = a + b + c + d;
+
+    printf("Sum of four numbers = %d\n", sum);
+
+    return 0;
+}
